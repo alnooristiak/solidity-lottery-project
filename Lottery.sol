4f6875f2c496db1f78b114b51c461e769070d9ee
Al Noor Istiak Mahmud <istiakmahmud1010@gmail.com>
2025-06-12 20:57:37 +0600

    selectWinner

---
+++ Lottery.sol
@@ -0,0 +1,30 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity  >=0.5.2 <0.9.0;
+
+contract Lottery{
+    address public manager;
+    address payable[] public participants;
+
+    constructor(){
+        manager =msg.sender; // gloabla variable
+    }
+    receive() external payable { 
+        require(msg.value==1 ether);
+        participants.push(payable(msg.sender));
+    }
+    function getBalance() public view returns(uint){
+        require(msg.sender== manager);
+        return address(this).balance; // gives us the balance of our address in wei
+    }
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.prevrandao,block.timestamp,participants.length)));
+    }
+    function selectWinner() public view returns(address){
+        require(msg.sender==manager);
+        require(participants.length>=3);
+        uint r=random();
+        uint index= r % participants.length;
+
+    }
+}
